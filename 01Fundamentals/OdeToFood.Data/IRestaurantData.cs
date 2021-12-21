2ec8b521c89b3ac76a3794900c7b73b80a453955
ikaroamorim <ikaro.amorim@gmail.com>
2021-12-20 21:48:08 -0300

    01Fundamentals -  Tests with mocked data

---
+++ 01Fundamentals/OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,36 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Data
+{
+  public interface IRestaurantData
+  {
+    IEnumerable<Restaurant> GetAll();
+  }
+
+  public class InMemoryRestaurantData : IRestaurantData
+  {
+    List<Restaurant> restaurants;
+
+    public InMemoryRestaurantData()
+    {
+      restaurants = new List<Restaurant>()
+      {
+        new Restaurant { Id=1, Name = "Scott's Pizza", Location="Maryland", Cuisine=CuisineType.Italian},
+        new Restaurant { Id = 2, Name = "Mc Donalds", Location = "São Paulo", Cuisine = CuisineType.Indian },
+        new Restaurant { Id = 3, Name = "Burger King", Location = "Campinas", Cuisine = CuisineType.Brazilian }
+      };
+    }
+
+    public IEnumerable<Restaurant> GetAll()
+    {
+      return from r in restaurants
+             orderby r.Name
+             select r;
+    }
+  }
+}
--- 01Fundamentals/OdeToFood/Startup.cs
+++ 01Fundamentals/OdeToFood/Startup.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using OdeToFood.Data;
 
 namespace OdeToFood
 {
@@ -24,6 +25,8 @@ namespace OdeToFood
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddRazorPages();
+
+      services.AddSingleton<IRestaurantData, InMemoryRestaurantData>();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
